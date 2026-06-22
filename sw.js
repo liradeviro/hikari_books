ffda009e3a7b9d46f412e4ebd0ad277e824933ad
liradeviro <anmirinova@gmail.com>
2026-06-22 09:29:02 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'hikari-v1';
+const FILES = [
+  '/hikari-books/',
+  '/hikari-books/index.html',
+  '/hikari-books/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
